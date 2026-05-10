17b10ae425d044e332ab3140e2ddff555bf720dd
LibraHo <154479797@qq.com>
2026-05-10 12:32:54 +0800

    test: use esm imports for ip location service

--- tests/unit/server/IpLocationService.test.js
+++ tests/unit/server/IpLocationService.test.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 const { IpLocationService } = require('../../../server/services/IpLocationService');
 
 describe('IpLocationService', () => {
